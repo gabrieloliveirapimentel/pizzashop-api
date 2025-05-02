ed8325520e60f302bb2e99030347a4eb5ec5c523
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-05-02 09:13:12 -0300

    Variáveis Ambiente no Bun

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
 import type { Config } from "drizzle-kit";
+import { env } from "./src/env";
 
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    host: "localhost",
-    port: 5432,
-    user: "docker",
-    password: "docker",
-    database: "pizzashop",
+    host: env.DATABASE_HOST,
+    port: env.DATABASE_PORT,
+    user: env.DATABASE_USER,
+    password: env.DATABASE_PASSWORD,
+    database: env.DATABASE_NAME,
   }
 } satisfies Config;
