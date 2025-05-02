46d9ad45397b7a66b40164540fda60d3efb7845b
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-05-02 08:51:41 -0300

    Migrations no Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,11 @@ export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: "postgresql",
+  dbCredentials: {
+    host: "localhost",
+    port: 5432,
+    user: "docker",
+    password: "docker",
+    database: "pizzashop",
+  }
 } satisfies Config;
