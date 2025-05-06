5ed1de09eaa8c434a891aaf9b7689d949c50006b
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-05-06 15:18:16 -0300

    Rota: Cadastro de restaurante

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,31 @@
 import { Elysia } from "elysia";
+import { db } from "../db/connection";
+import { restaurants, users } from "../db/schema";
 
 const app = new Elysia();
 
-app.get("/", () => "Hello Elysia!");
+app.post("/restaurants", async ({ body, set }) => {
+	const { restaurantName, name, email, phone } = body as any;
+
+	const [manager] = await db
+		.insert(users)
+		.values({
+			name,
+			email,
+			phone,
+			role: "manager",
+		})
+		.returning({
+			id: users.id,
+		});
+
+	await db.insert(restaurants).values({
+		name: restaurantName,
+		managerId: manager?.id,
+	});
+
+	set.status = 204;
+});
 
 app.listen(3333, () => {
 	console.log("HTTP server running!");
